89a84af886682f0c563936ac306330d3031a4726
Canyi Lu <canyilu@gmail.com>
2018-06-18 21:41:29 -0400

    Add files via upload

--- tprod.m
+++ tprod.m
@@ -1,6 +1,6 @@
 function C = tprod(A,B)
 
-% Tensor-Tensor product of two 3-way tensor: C = A*B
+% Tensor-tensor product of two 3 way tensors: C = A*B
 % A - n1*n2*n3 tensor
 % B - n2*l*n3  tensor
 % C - n1*l*n3  tensor
@@ -8,13 +8,27 @@ function C = tprod(A,B)
 % version 2.0 - 09/10/2017
 %
 % Written by Canyi Lu (canyilu@gmail.com)
-% 
+%
+%
+% References: 
+% Canyi Lu, Tensor-Tensor Product Toolbox. Carnegie Mellon University. 
+% June, 2018. https://github.com/canyilu/tproduct.
+%
+% Canyi Lu, Jiashi Feng, Yudong Chen, Wei Liu, Zhouchen Lin and Shuicheng
+% Yan, Tensor Robust Principal Component Analysis with A New Tensor Nuclear
+% Norm, arXiv preprint arXiv:1804.03728, 2018
+%
 
-[n1,~,n3] = size(A);
-l = size(B,2);
+[n1,n2,n3] = size(A);
+[m1,m2,m3] = size(B);
+
+if n2 ~= m1 || n3 ~= m3 
+    error('Inner tensor dimensions must agree.');
+end
+
 A = fft(A,[],3);
 B = fft(B,[],3);
-C = zeros(n1,l,n3);
+C = zeros(n1,m2,n3);
 
 % first frontal slice
 C(:,:,1) = A(:,:,1)*B(:,:,1);
@@ -31,5 +45,3 @@ if mod(n3,2) == 0
     C(:,:,i) = A(:,:,i)*B(:,:,i);
 end
 C = ifft(C,[],3);
-
-
--- tubalrank.m
+++ tubalrank.m
@@ -1,30 +1,43 @@
-function trank = tubalrank(X)
+function trank = tubalrank(X,tol)
 
-% The tubal rank of the tensor nuclear norm of a 3 way tensor
+% The tensor tubal rank of a 3 way tensor
 %
 % X     -    n1*n2*n3 tensor
 % trank -    tensor tubal rank of X
 %
-% version 1.0 - 15/09/2017
+% version 2.0 - 14/06/2018
 %
 % Written by Canyi Lu (canyilu@gmail.com)
-% 
+%
+%
+% References: 
+% Canyi Lu, Tensor-Tensor Product Toolbox. Carnegie Mellon University. 
+% June, 2018. https://github.com/canyilu/tproduct.
+%
+% Canyi Lu, Jiashi Feng, Yudong Chen, Wei Liu, Zhouchen Lin and Shuicheng
+% Yan, Tensor Robust Principal Component Analysis with A New Tensor Nuclear
+% Norm, arXiv preprint arXiv:1804.03728, 2018
+%
 
-n3 = size(X,3);
 X = fft(X,[],3);
+[n1,n2,n3] = size(X);
+s = zeros(min(n1,n2),1);
 
-trank = 0;
-% first frontal slice
-trank = max(trank,rank(X(:,:,1)));
-
+% i=1
+s = s + svd(X(:,:,1),'econ');
 % i=2,...,halfn3
 halfn3 = round(n3/2);
 for i = 2 : halfn3
-    trank = max(trank,rank(X(:,:,i)));
+    s = s + svd(X(:,:,i),'econ')*2;
 end
-
 % if n3 is even
 if mod(n3,2) == 0
     i = halfn3+1;
-    trank = max(trank,rank(X(:,:,i)));    
+    s = s + svd(X(:,:,i),'econ');
 end
+s = s/n3;
+
+if nargin==1
+   tol = max(n1,n2) * eps(max(s));
+end
+trank = sum(s > tol);
